641a094bc75c9b4bbd847e4d21d9804f8f0b9286
Luis Morneno <morenoluise535@gmail.com>
2018-12-19 19:42:18 -0800

    server update v2

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var path = require('path')
 
-var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
